c44b454d6d8d11444e76e8a33ff889b90b131bd7
Devarshi <devarshi@atyantik.com>
2024-02-29 14:20:34 +0530

    integrated github and google both

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,20 @@
 import { defineConfig } from 'astro/config';
 import auth from 'auth-astro';
-import vercel from '@astrojs/vercel/serverless';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [auth()],
   output: 'server',
-  adapter: vercel(),
+  adapter: node({
+  mode: 'standlone',
+  }),
   build: {
     rollupOPtions: {
         external: ['@auth/core'],
     },
   },
   targets: {
-    vercel,
+    node,
   },
 });
--- auth.config.mjs
+++ auth.config.mjs
@@ -1,8 +1,13 @@
 import GitHub from '@auth/core/providers/github';
+import Google from '@auth/core/providers/google';
 import { defineConfig } from 'auth-astro';
 
 export default defineConfig({
   providers: [
+    Google({
+      clientId: import.meta.env.GOOGLE_CLIENT_ID,
+      clientSecret: import.meta.env.GOOGLE_CLIENT_SECRET,
+    }),
     GitHub({
       clientId: import.meta.env.GITHUB_CLIENT_ID,
       clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
