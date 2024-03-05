561fa7d72d86284ba144836ee571bc0997cbe8d4
ramkrishnabhatt <ramkrishna@atyantik.com>
2024-03-05 09:51:21 +0530

    added graphql

---
+++ src/services/todos.ts
@@ -0,0 +1,27 @@
+import { gql } from "@apollo/client/core";
+
+const QUERY_TODO_LIST = gql`
+  query getSingleUser($userId: String!) {
+    getUserTodo(userId: $userId) {
+      status
+      title
+    }
+  }
+`;
+
+const ADD_TODO = gql`
+  mutation UpdateTodoList($input: UpdateTodoList!) {
+    updateTodoList(input: $input) {
+      id
+      status
+      title
+      userId
+    }
+  }
+`;
+
+export {
+  QUERY_TODO_LIST,
+  ADD_TODO,
+};
+ 
